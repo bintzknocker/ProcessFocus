3ad97cad786180b033298654191636d4e2bffd18
Bintzknocker <bintzknocker@users.noreply.github.com>
2024-11-22 10:31:22 -0600

    initial checkin

---
+++ ProcessFocusSystemTray/ProcessFocus/ProcessFocusApplicationContext.cs
@@ -0,0 +1,70 @@
+﻿using ProcessFocus.Services;
+
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProcessFocus
+{
+    public class ProcessFocusApplicationContext : ApplicationContext
+    {
+        #region ..Private Static
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static int WAITTIME = int.Parse(ConfigurationManager.AppSettings["waitTime"]);
+        private static string APPNAME = ConfigurationManager.AppSettings["appName"];
+        #endregion
+
+        #region ..Private
+        private NotifyIcon trayIcon;
+        #endregion
+
+        #region ..Public Methods
+        public ProcessFocusApplicationContext()
+        {
+            log.Info("init");
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = ProcessFocus.AppResources.MainIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Exit", Exit)
+            }),
+                Visible = true
+            };
+
+
+            ProcessWatcher procWatcher = new ProcessWatcher(APPNAME);
+            procWatcher.ProcessCreated += ProcWatcher_ProcessCreated;
+            procWatcher.ProcessDeleted += ProcWatcher_ProcessDeleted;
+            procWatcher.Start();
+        }
+        #endregion
+        
+        #region ..Events
+        private static void ProcWatcher_ProcessDeleted(Win32_Process proc)
+        {
+            log.Info($"PROCESS {APPNAME} Deleted");
+        }
+
+        private static void ProcWatcher_ProcessCreated(Win32_Process proc)
+        {
+            log.Info($"PROCESS {APPNAME} Created, waiting for {WAITTIME} to bring to front");
+            Thread.Sleep(WAITTIME);
+            log.Info($"    moving PROCESS {APPNAME} to front");
+
+            var currentProcess = Process.GetProcessById(int.Parse(proc.Handle));
+            WindowHelper.bringToFront(currentProcess.MainWindowHandle);
+        }        
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+        #endregion
+    }
+}
---
+++ ProcessFocusSystemTray/ProcessFocus/Services/WindowHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ProcessFocus.Services
+{
+    public static class WindowHelper
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private const int ALT = 0xA4;
+        private const int EXTENDEDKEY = 0x1;
+        private const int KEYUP = 0x2;
+        private const uint Restore = 9;
+
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern bool AllowSetForegroundWindow(int procID);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        public static void bringToFront(IntPtr handle)
+        {
+            log.Info($"incoming handle - {handle}");
+
+            // Verify that app is a running process.
+            if (handle == IntPtr.Zero)
+            {
+                log.Info($"incoming handle is zero, returning");
+                return;
+            }
+
+
+            var foregroundWindow = GetForegroundWindow();
+            log.Info($"foregroundWindow - {foregroundWindow}");
+
+
+            //if already in front, get out
+            if (handle == foregroundWindow)
+            {
+                log.Info($"handle is foreground window, returning");
+                return;
+            }
+
+            log.Info($" keydown");
+
+            //AllowSetForegroundWindow(handle.ToInt32());
+            // Simulate a key press
+            keybd_event((byte)ALT, 0x45, EXTENDEDKEY | 0, 0);
+
+            //SetForegroundWindow(mainWindowHandle);
+
+            log.Info($" keyup");
+            // Simulate a key release
+            keybd_event((byte)ALT, 0x45, EXTENDEDKEY | KEYUP, 0);
+
+            // Make app the foreground application
+            SetForegroundWindow(handle);
+
+            log.Info($" after setforegroundwindow");
+        }
+    }
+}
